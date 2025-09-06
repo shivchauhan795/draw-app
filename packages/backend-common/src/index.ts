6bb1c178cc87b5d273c1b72a1b845877f68e143a
Shiv Chauhan <shivchauhan795@gmail.com>
2025-09-06 12:53:35 +0530

    fixed env handling for backend common package

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,1 +1,14 @@
+import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, '../.env') });
+
+console.log("env", process.env.JWT_SECRET);
+
+
+
 export const JWT_SECRET = process.env.JWT_SECRET!;
